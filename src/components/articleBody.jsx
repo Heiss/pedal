663c620f71f6b24dd359fee31d43278d1bf3b6df
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-16 20:52:20 +0200

    hide image, if it is undefined

--- src/components/articleBody.jsx
+++ src/components/articleBody.jsx
@@ -2,21 +2,20 @@ import Profile from "../components/profile";
 import { Raw, Fragment } from '@bikeshaving/crank';
 
 
-export default function ({ title, body, image = "", category = "Misc" }) {
+export default function ({ title, body, image = undefined, category = "Misc" }) {
+    var showImage = (image !== undefined)
+
     return (
         <Fragment>
             <div class="px-4 lg:px-0">
                 <h2 class="text-4xl font-semibold text-gray-800 leading-tight">
                     {title}
                 </h2>
-                <a
-                    href="#"
-                    class="py-2 text-green-700 inline-flex items-center justify-center mb-2"
-                >
+                <a href="#" class="py-2 text-green-700 inline-flex items-center justify-center mb-2">
                     {category}
                 </a>
 
-                <img src="{image}" class="w-full object-cover lg:rounded" style="height: 28em;" />
+                {showImage ? <img src="{image}" class="w-full object-cover lg:rounded" style="height: 28em;" /> : ""}
             </div>
 
             <div class="flex flex-col lg:flex-row lg:space-x-12">
